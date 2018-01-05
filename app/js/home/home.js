1a507f1b294be0030d3a39d4bef933b8fe944032
Ivange Larry <ivangelarry@gmail.com>
2018-01-05 15:09:01 +0100

    Delete unnecessary files

--- app/js/home/home.js
+++ app/js/home/home.js
@@ -1,15 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import breadcrumbsComponent from './components/breadcrumbs.component.js';
-import breadcrumbsPatientCreateComponent from './components/breadcrumbsPatientCreate.component';
-import translateComponent from './components/translate.component.js';
 import headerComponent from './components/header.component.js';
-import notificationComponent from './components/notification.component.js';
-import patientSearchComponent from './components/patientSearch.component.js';
-import encounterComponent from './components/encounter.component.js';
-import observationComponent from './components/observation.component.js';
-import providerComponent from './components/provider.component.js';
-import patientCreateComponent from './components/patientCreate.component.js';
 import uicommons from 'openmrs-contrib-uicommons';
 
 let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons'])
@@ -21,11 +13,6 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
             url: '/',
             template: require('./home.html')
         });
-
-        $stateProvider.state('createPatient', {
-        url: '/create-patient',
-        template: require('./patientCreatePage.html')
-        })
     })
     .config(['$qProvider', function ($qProvider) {
       $qProvider.errorOnUnhandledRejections(false);
@@ -38,14 +25,6 @@ let homeModule = angular.module('home', [ uiRouter, 'openmrs-contrib-uicommons']
     }])
 
     .component('breadcrumbsComponent', breadcrumbsComponent)
-    .component('breadcrumbsPatientCreateComponent', breadcrumbsPatientCreateComponent)
-    .component('translateComponent', translateComponent)
-    .component('headerComponent', headerComponent)
-    .component('notificationComponent', notificationComponent)
-    .component('patientSearchComponent', patientSearchComponent )
-    .component('encounterComponent', encounterComponent)
-    .component('observationComponent', observationComponent)
-    .component('providerComponent', providerComponent)
-    .component('patientCreateComponent', patientCreateComponent );
+    .component('headerComponent', headerComponent);
 
 export default homeModule;
